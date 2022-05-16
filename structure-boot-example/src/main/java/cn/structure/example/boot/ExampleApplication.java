57d26127390023fad6b8ebd659afc7892a1ce15f
chuckLcq <15524415221@163.com>
2022-05-16 20:53:57 +0800

    升级v1.1.0 并发布版本

---
+++ structure-boot-example/src/main/java/cn/structure/example/boot/ExampleApplication.java
@@ -0,0 +1,20 @@
+package cn.structure.example.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 启动器程序
+ * </p>
+ *
+ * @author chuck
+ * @version 1.0.1
+ * @since 2021/12/5 22:54
+ */
+@SpringBootApplication
+public class ExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
+}
---
+++ structure-boot-example/src/main/java/cn/structure/example/boot/TestController.java
@@ -0,0 +1,24 @@
+package cn.structure.example.boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 测试
+ * </p>
+ *
+ * @author chuck
+ * @version 1.0.1
+ * @since 2021/12/5 22:55
+ */
+@RestController
+@RequestMapping(value = "/")
+public class TestController {
+
+    @GetMapping(value = "test")
+    public String test(){
+        return "ok";
+    }
+}
